0cee9177c97c14f304e87392fc02e0f9f5a077a6
G1AN5 <gdbsaluta@mymail.mapua.edu.ph>
2025-05-28 22:29:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Global styles for the quiz
-import App from './App'; // Your main Quiz App component
+import './index.css'; 
+import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App /> {/* Our Quiz App */}
+        <App /> {}
     </React.StrictMode>
 );
