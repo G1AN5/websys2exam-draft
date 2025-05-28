4c93eaf565ddbcbd5f9776985b62298fd2c743ae
G1AN5 <gdbsaluta@mymail.mapua.edu.ph>
2025-05-28 22:30:32 +0800

    Update Identification.js

--- src/components/Identification.js
+++ src/components/Identification.js
@@ -1,12 +1,10 @@
-// src/components/Identification.js - Only showing relevant style changes
-
 import React from 'react';
 
 function Identification({ questionNumber, question, currentAnswer, setTextInput }) {
     const questionStyle = {
-        fontSize: '1.4em', // Adjusted
+        fontSize: '1.4em', 
         fontWeight: 'bold',
-        marginBottom: '18px', // Adjusted
+        marginBottom: '18px', 
         color: '#333',
         textAlign: 'left',
         width: '100%',
@@ -15,13 +13,13 @@ function Identification({ questionNumber, question, currentAnswer, setTextInput 
 
     const inputStyle = {
         width: '100%',
-        padding: '12px', // Slightly less padding
-        fontSize: '1.0em', // Adjusted
+        padding: '12px', 
+        fontSize: '1.0em', 
         borderRadius: '5px',
         border: '1px solid #ccc',
         boxShadow: 'inset 0 1px 3px rgba(0,0,0,0.1)',
         boxSizing: 'border-box',
-        marginBottom: '18px', // Adjusted
+        marginBottom: '18px', 
     };
 
     return (
